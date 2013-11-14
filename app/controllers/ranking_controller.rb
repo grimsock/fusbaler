7f56bbd4442db5ee21a88d7f97ae72a47ff274eb
Wojciech Ochmański <wojciech.ochmanski@llp.pl>
2013-11-14 18:24:07 +0100

    fix ranking positions sorting

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,5 +1,6 @@
 class RankingController < ApplicationController
   def index
-    @ranking_positions = Ranking.default.first.ranking_positions.order(:rank)
+    @ranking_positions = Ranking.default.first.ranking_positions.order(rank: :desc)
+    p @ranking_positions
   end
 end
