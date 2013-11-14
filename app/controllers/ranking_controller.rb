01340f7ac9e97b3d01faed823014064609e2a846
Wojciech Ochmański <wojciech.ochmanski@llp.pl>
2013-11-14 18:45:54 +0100

    remove puts

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,6 +1,5 @@
 class RankingController < ApplicationController
   def index
     @ranking_positions = Ranking.default.first.ranking_positions.order(rank: :desc)
-    p @ranking_positions
   end
 end
