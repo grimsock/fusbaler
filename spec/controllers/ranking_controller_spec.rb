d7bbf2e5d960f557cf7a2bb405ac796cf257ddf2
Wojciech Ochmański <wojciech.ochmanski@llp.pl>
2013-11-14 18:27:53 +0100

    check in ranking controller spec for sorting order

--- spec/controllers/ranking_controller_spec.rb
+++ spec/controllers/ranking_controller_spec.rb
@@ -3,16 +3,15 @@ require 'spec_helper'
 describe RankingController do
   describe '#index' do
     context 'when teams exist' do
-      let(:ranking) { build(:default_ranking) }
-      let(:ranking_position_1) { build(:ranking_position, ranking: ranking) }
-      let(:ranking_position_2) { build(:ranking_position, ranking: ranking) }
+      let(:ranking) { create(:default_ranking) }
+      let!(:ranking_position_1) { create(:ranking_position, rank: 1, ranking: ranking) }
+      let!(:ranking_position_2) { create(:ranking_position, rank: 3, ranking: ranking) }
       before do
-        Ranking.stub_chain(:default, :first, :ranking_positions, :order).and_return([ranking_position_1, ranking_position_2])
         get :index
       end
 
       it { expect(response).to be_success }
-      it { expect(assigns(:ranking_positions)).to eq([ranking_position_1, ranking_position_2]) }
+      it { expect(assigns(:ranking_positions)).to eq([ranking_position_2, ranking_position_1]) }
     end
 
     context 'when no ranking positions' do
