deaf70f52bf8f64c44d79a9684ec7acc9cb62e3c
Wojciech Ochmański <wojciech.ochmanski@llp.pl>
2013-11-21 10:28:01 +0100

    ranking publicly visible

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,4 +1,6 @@
 class RankingController < ApplicationController
+  skip_before_filter :authorize_user
+
   def index
     @ranking_positions = Ranking.default.first.ranking_positions.order(rank: :desc)
   end
